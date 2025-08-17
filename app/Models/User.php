beb3adc3aa7e5f6a704f2248f27145e37c4c5692
mithayuliantari <kadekmitha21@gmail.com>
2025-08-18 06:21:42 +0800

    update user

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function canAccessFilament(): bool
+    {
+        return true;
+    }
+
 }
