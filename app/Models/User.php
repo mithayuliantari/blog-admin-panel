e43102b4204e6657d3cba8b36967f31b26fd9298
mithayuliantari <kadekmitha21@gmail.com>
2025-08-18 06:44:00 +0800

    update admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,7 +33,7 @@ class User extends Authenticatable implements FilamentUser
     }
 
 
-    public function canAccessPanel(Panel $panel): bool
+    public function canAccessPanel(\Filament\Panel $panel): bool
     {
         return true; // izinkan semua user
     }
