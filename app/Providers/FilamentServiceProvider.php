74eb940579d4f4fb432b42ab25483dd94934f261
mithayuliantari <kadekmitha21@gmail.com>
2025-08-18 23:30:06 +0800

    Fix admin access and Filament provider logic

--- app/Providers/FilamentServiceProvider.php
+++ app/Providers/FilamentServiceProvider.php
@@ -2,13 +2,28 @@
 
 namespace App\Providers;
 
-use Filament\Support\Facades\FilamentAsset;
+use Filament\Facades\Filament;
 use Illuminate\Support\ServiceProvider;
+use Filament\Support\Facades\FilamentAsset;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
 
 class FilamentServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        // ⛔️ Block non-admin users from accessing Filament
+
+        Filament::serving(function () {
+            /** @var User|null $user */
+            $user = Auth::user();
+
+            if (! $user?->isAdmin()) {
+                abort(403);
+            }
+        });
+
         // Force HTTPS untuk Filament assets
         if (app()->environment('production')) {
             FilamentAsset::register([
